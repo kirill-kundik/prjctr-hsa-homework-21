905aea9cec3dd2c135808cdced39b9a1956de4c3
Kyrylo Kundik <goliathusua@gmail.com>
2023-05-12 00:01:05 +0300

    created mixture of horizontal and vertical sharding using postgres;

---
+++ initdb.d/shard_1.sql
@@ -0,0 +1,46 @@
+CREATE EXTENSION postgres_fdw;
+
+CREATE SERVER shard_2_server
+FOREIGN DATA WRAPPER postgres_fdw
+OPTIONS (host 'postgres2', port '5432', dbname 'shard_db');
+
+CREATE SERVER shard_3_server
+FOREIGN DATA WRAPPER postgres_fdw
+OPTIONS (host 'postgres3', port '5432', dbname 'shard_db');
+
+CREATE USER MAPPING FOR shard_user
+SERVER shard_2_server
+OPTIONS (user 'shard_user', password 'shard_pass');
+
+CREATE USER MAPPING FOR shard_user
+SERVER shard_3_server
+OPTIONS (user 'shard_user', password 'shard_pass');
+
+CREATE TABLE IF NOT EXISTS measurements
+(
+    city_id INT NOT NULL,
+    logdate DATE NOT NULL,
+    peaktemp INT NOT NULL,
+    unitsales INT NOT NULL
+) PARTITION BY RANGE (logdate);
+
+CREATE INDEX idx_measurements_logdate ON measurements (logdate);
+
+CREATE TABLE measurements_2023 PARTITION OF measurements
+FOR VALUES FROM ('2023-01-01') TO ('2024-01-01');
+
+CREATE TABLE measurements_2010_2020 PARTITION OF measurements
+FOR VALUES FROM ('2010-01-01') TO ('2021-01-01');
+
+CREATE TABLE measurements_default PARTITION OF measurements
+DEFAULT;
+
+CREATE FOREIGN TABLE measurements_2022
+PARTITION OF measurements
+FOR VALUES FROM ('2022-01-01') TO ('2023-01-01')
+SERVER shard_2_server OPTIONS (table_name 'measurements');
+
+CREATE FOREIGN TABLE measurements_2021
+PARTITION OF measurements
+FOR VALUES FROM ('2021-01-01') TO ('2022-01-01')
+SERVER shard_3_server OPTIONS (table_name 'measurements');
